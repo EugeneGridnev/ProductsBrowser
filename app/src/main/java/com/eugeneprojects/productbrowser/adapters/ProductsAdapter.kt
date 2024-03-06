9ab168b7f87154181cebed4652291ebe511dfdd7
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-06 19:37:59 +0300

    product adapter added

---
+++ app/src/main/java/com/eugeneprojects/productbrowser/adapters/ProductsAdapter.kt
@@ -0,0 +1,51 @@
+package com.eugeneprojects.productbrowser.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.eugeneprojects.productbrowser.databinding.ItemProductLayoutBinding
+import com.eugeneprojects.productbrowser.models.Product
+
+class ProductsAdapter : RecyclerView.Adapter<ProductsAdapter.ProductViewHolder>() {
+
+    inner class ProductViewHolder(val binding: ItemProductLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private  val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder(
+            ItemProductLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+
+        holder.itemView.apply {
+            val product = differ.currentList[position]
+            Glide.with(this).load(product.thumbnail).into(holder.binding.ivProductThumbnail)
+            holder.binding.tvProductTitle.text = product.title
+            holder.binding.tvProductDescription.text = product.description
+        }
+    }
+}
