48e818dd2ed58cf8a8008c8d4611dd3d4255d62b
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-09 22:26:29 +0300

    paging load sate handled

--- app/src/main/java/com/eugeneprojects/productbrowser/ui/fragments/ProductsListFragment.kt
+++ app/src/main/java/com/eugeneprojects/productbrowser/ui/fragments/ProductsListFragment.kt
@@ -5,23 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.eugeneprojects.productbrowser.adapters.ProductsLoadStateAdapter
 import com.eugeneprojects.productbrowser.adapters.ProductsPagingAdapter
 import com.eugeneprojects.productbrowser.databinding.FragmentProductsListBinding
 import com.eugeneprojects.productbrowser.repository.ProductsRepositoryIMPL
 import com.eugeneprojects.productbrowser.ui.ProductsListViewModel
 import com.eugeneprojects.productbrowser.ui.ProductsViewModelProviderFactory
+import com.eugeneprojects.productbrowser.util.Constants
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class ProductsListFragment : Fragment() {
 
     private var binding: FragmentProductsListBinding? = null
-    private lateinit var productsPagingAdapter: ProductsPagingAdapter
     private lateinit var viewModel: ProductsListViewModel
 
     override fun onCreateView(
@@ -34,18 +38,40 @@ class ProductsListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val productsRepository = ProductsRepositoryIMPL()
         val viewModelProviderFactory = ProductsViewModelProviderFactory(productsRepository)
 
-        viewModel = ViewModelProvider(this, viewModelProviderFactory)[ProductsListViewModel::class.java]
+        viewModel =
+            ViewModelProvider(this, viewModelProviderFactory)[ProductsListViewModel::class.java]
 
-        setUpRecyclerView()
-        observeProducts(productsPagingAdapter)
+        setUpProductsList()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun setUpProductsList() {
+
+        val productsPagingAdapter = ProductsPagingAdapter()
+
+        productsPagingAdapter.withLoadStateFooter(ProductsLoadStateAdapter())
+
+        binding?.recyclerViewProducts?.layoutManager = LinearLayoutManager(activity)
+        binding?.recyclerViewProducts?.adapter = productsPagingAdapter
+
+        productsPagingAdapter.addLoadStateListener { combinedLoadStates ->
+            val refreshState = combinedLoadStates.refresh
+            binding?.recyclerViewProducts?.isVisible = refreshState != LoadState.Loading
+            binding?.progressBar?.isVisible = refreshState == LoadState.Loading
+            if (refreshState is LoadState.Error) {
+                Toast.makeText(activity, Constants.TOAST_ERROR, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        observeProducts(productsPagingAdapter)
     }
 
     private fun observeProducts(adapter: ProductsPagingAdapter) {
@@ -55,13 +81,4 @@ class ProductsListFragment : Fragment() {
             }
         }
     }
-
-    private fun setUpRecyclerView() {
-        productsPagingAdapter = ProductsPagingAdapter()
-        binding?.rvProducts?.apply {
-            adapter = productsPagingAdapter
-            layoutManager = LinearLayoutManager(activity)
-        }
-    }
-
 }
--- app/src/main/java/com/eugeneprojects/productbrowser/util/Constants.kt
+++ app/src/main/java/com/eugeneprojects/productbrowser/util/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
 
     const val BASE_URL = "https://dummyjson.com"
     const val PAGE_SIZE = 20
+    const val TOAST_ERROR = "Ошибка загрузки"
 
 }
