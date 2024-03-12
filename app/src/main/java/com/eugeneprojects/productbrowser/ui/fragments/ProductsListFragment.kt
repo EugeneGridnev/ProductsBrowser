503318dafca1263710d5d9c117c17946a8df6b5c
EugeneGridnev <eugenegrid@yandex.ru>
2024-03-12 20:37:36 +0300

    code clean up

--- app/src/main/java/com/eugeneprojects/productbrowser/ui/fragments/ProductsListFragment.kt
+++ app/src/main/java/com/eugeneprojects/productbrowser/ui/fragments/ProductsListFragment.kt
@@ -1,13 +1,12 @@
 package com.eugeneprojects.productbrowser.ui.fragments
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
@@ -51,7 +50,7 @@ class ProductsListFragment : Fragment() {
 
         setUpProductsList()
         observeProducts()
-        setTextChangeListener()
+        handleSearchChanges()
         handleScrollingToTopWhenSearching(productsPagingAdapter)
 
         viewModel.isOnline.observe(viewLifecycleOwner) { isOnline ->
@@ -134,18 +133,11 @@ class ProductsListFragment : Fragment() {
         binding?.swipeRefresh?.setOnRefreshListener { adapter.refresh() }
     }
 
-    private fun setTextChangeListener() {
+    private fun handleSearchChanges() {
 
-        binding?.searchEditText?.addTextChangedListener(object: TextWatcher {
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.setSearchQuery(s.toString())
-            }
-
-            override fun afterTextChanged(s: Editable?) { }
-        })
+        binding?.searchEditText?.doOnTextChanged { searchQuery, _, _, _ ->
+            viewModel.setSearchQuery(searchQuery.toString())
+        }
     }
 
     private fun handleScrollingToTopWhenSearching(adapter: ProductsPagingAdapter) = lifecycleScope.launch {
